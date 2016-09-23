f6fd5e973bcfa5988f5ff731804330af45c65f32
eksana <ser.vajnrub@yandex.ru>
2016-09-23 16:53:04 +0400

    ch

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,10 @@ var gulp = require('gulp'),
 /*gulp.task('mytask', function() {
     console.log('Привет, я таск!');
 });*/
- scss = require('gulp-scss'); //Подключаем Sass пакет
+ sass = require('gulp-sass'); //Подключаем Sass пакет
+
+ gulp.task('sass', function(){ // Создаем таск "sass"
+    return gulp.src('src/sass/main.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('src/css')) // Выгружаем результата в папку app/css
+});
