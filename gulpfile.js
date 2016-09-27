70b72ac98adaece432539399419f291c67014575
eksana <ser.vajnrub@yandex.ru>
2016-09-27 13:23:08 +0400

    ch

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,21 @@ var gulp = require('gulp'),
     console.log('Привет, я таск!');
 });*/
  sass = require('gulp-sass'); //Подключаем Sass пакет
+ //sass = require('gulp-scss'); //Подключаем Sass пакет
 
  gulp.task('sass', function(){ // Создаем таск "sass"
-    return gulp.src('src/sass/main.scss') // Берем источник
+    return gulp.src('src/sass/**/*.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(gulp.dest('src/css')) // Выгружаем результата в папку app/css
 });
+
+ /*gulp.task('sass', function(){ // Создаем таск "sass"
+    return gulp.src('src/sass/bootstrap-grid.css') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('src/css')) // Выгружаем результата в папку app/css
+});
+gulp.task('sass', function(){ // Создаем таск "sass"
+    return gulp.src('src/sass/variables.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('src/css')) // Выгружаем результата в папку app/css
+});*/
