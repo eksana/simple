1c9275b51e6072210eb39edd2251f0ab8e112d3e
eksana <ser.vajnrub@yandex.ru>
2016-11-30 14:17:23 +0300

    ch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp'),
-    concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    uglify      = require('gulp-uglifyjs'),// Подключаем gulp-uglifyjs (для сжатия JS)
-    cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-    rename      = require('gulp-rename'), // Подключаем библиотеку для переименования
-    imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-    pngquant    = require('imagemin-pngquant'),
-    cache       = require('gulp-cache'), // Подключаем библиотеку кеширова
-    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автомат
+    //concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    //uglify      = require('gulp-uglifyjs'),// Подключаем gulp-uglifyjs (для сжатия JS)
+    //cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+   // rename      = require('gulp-rename'), // Подключаем библиотеку для переименования
+    //imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    //pngquant    = require('imagemin-pngquant'),
+   // cache       = require('gulp-cache'), // Подключаем библиотеку кеширова
+   autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автомат
 
 /*gulp.task('mytask', function() {
     console.log('Привет, я таск!');
@@ -41,21 +41,21 @@ gulp.task('css-libs', ['sass'], function() {
     return gulp.src(['src/css/bootstrap-grid.css',
                       'src/css/main.css',
                       'src/css/variables.css'
-        ]) 
+        ]) */
 
      //.pipe(concat('libs.css')) // Собираем их в кучу в новом файле libs.min.css 
-        .pipe(cssnano()) // Сжимаем
+        /*.pipe(cssnano()) // Сжимаем
         .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
         .pipe(gulp.dest('build/css')); // Выгружаем в папку app/css
 });*/
 
-gulp.task('img', function() {
-    return gulp.src('src/img/**/*') // Берем все изображения из app
-       .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
-            interlaced: true,
+/*gulp.task('img', function() {
+    return gulp.src('src/img/**///*') // Берем все изображения из app
+       //.pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+           /* interlaced: true,
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         })))
         .pipe(gulp.dest('build/img')); // Выгружаем на продакшен
-});
+});*/
