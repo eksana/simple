f9ae72cf1c253331b7f3e89efce4b832448b3438
eksana <ser.vajnrub@yandex.ru>
2016-12-05 11:43:13 +0300

    ch

--- src/js/common.js
+++ src/js/common.js
@@ -89,3 +89,23 @@ $('.home-nav-sm').slideToggle();
 		return false;
 	});		
 	});
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay: 6000 
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
