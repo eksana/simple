27850301fedd0fe6a8e68e4073ac4305411ce051
eksana <ser.vajnrub@yandex.ru>
2016-12-14 17:49:06 +0300

    ch
    ch

--- src/js/common.js
+++ src/js/common.js
@@ -73,6 +73,25 @@ $('.home-nav-sm').slideToggle();
 		}, 800);
 		return false;
 	});
+
+	//topUp
+	
+  $(document).ready(function() {
+var top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+  var delay = 1000; // Задержка прокрутки
+  	
+    $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+      /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+      if ($(this).scrollTop() > top_show) $('#top').fadeIn();
+      else $('#top').fadeOut();
+    });
+    $('#top').click(function () { // При клике по кнопке "Наверх" попадаем в эту функцию
+      /* Плавная прокрутка наверх */
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
+  });
 	
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
