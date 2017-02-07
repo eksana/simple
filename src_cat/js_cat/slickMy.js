680617eb45105dbd80ec7396f7d17cd3b6e0aa44
eksana <ser.vajnrub@yandex.ru>
2017-02-07 16:40:14 +0300

    add testimonials

--- src_cat/js_cat/slickMy.js
+++ src_cat/js_cat/slickMy.js
@@ -1,10 +1,33 @@
 
     $(document).ready(function(){
-      $('.multiple-items').slick({
+      $('.center').slick({
+      	autoplay:true,
+      	
+      	arrows: true,
 
-  infinite: true,
+  centerMode: true,
+  centerPadding: '60px',
   slidesToShow: 3,
-  slidesToScroll: 3
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
        
     });
